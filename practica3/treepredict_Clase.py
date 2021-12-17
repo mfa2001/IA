efa13593c6120acbac6696ab75acac2212037041
mfa2001 <39986028+mfa2001@users.noreply.github.com>
2021-12-17 11:18:17 +0100

    Tree predict mio

--- practica3/treepredict_Clase.py
+++ practica3/treepredict_Clase.py
@@ -1,32 +1,39 @@
 #!/usr/bin/env python3
 from math import log
-from typing import List, Tuple, Dict
-import sys
+from typing import List, Tuple
 from collections import Counter
+import sys
 
 # Used for typing
 Data = List[List]
 
+
 def _parse_value(value: str):
     try:
-        return float(value)
+        if float(value) == int(value):
+            return int(value)
+        else:
+            return float(value)
     except ValueError:
         return value
 
-def read(file_name: str) -> Tuple[List[str], Data]:
+
+def read(file_name: str, separator: str = ",") -> Tuple[List[str], Data]:
     """
     t3: Load the data into a bidimensional list.
     Return the headers as a list, and the data
     """
+    headers = None
     data = list()
-    with open(file_name, "r") as file_:
-        headers = file_.readline().strip("\n").split(",")
-        for row in file_:
-            values = row.strip("\n").split(",")
-            values = [_parse_value(v) for v in values]
-            data.append(values)
-    return headers,data
+    with open(file_name) as file_:
+        for line in file_:
+            values = line.strip().split(separator)
+            if not headers:
+                headers = values
+            else:
+                data.append([_parse_value(v) for v in values])
 
+    return headers, data
 
 
 def unique_counts(part: Data):
@@ -34,12 +41,19 @@ def unique_counts(part: Data):
     t4: Create counts of possible results
     (the last column of each row is the
     result)
-    """     
+    """
     results = Counter()
     for row in part:
         label = row[-1]
-        results[label]+= 1
+        results[label] += 1
     return results
+    """
+    # Alternative using the counter directly
+    labels = list()
+    for row in part:
+        labels.append(row[-1])
+    return Counter(labels)
+    """
 
 
 def gini_impurity(part: Data):
@@ -49,15 +63,16 @@ def gini_impurity(part: Data):
     total = len(part)
     if total == 0:
         return 0
+
     results = unique_counts(part)
     imp = 1
-    for value in results.values():
-        p = (value/ total)
+    for count in results.values():
+        p = count / total
         imp -= p**2
     return imp
 
 
-def _log2(value: float):
+def _log2(value: int):
     return log(value) / log(2)
 
 
@@ -66,22 +81,22 @@ def entropy(rows: Data):
     t6: Entropy is the sum of p(x)log(p(x))
     across all the different possible results
     """
-    results = unique_counts(rows)
     total = len(rows)
+    results = unique_counts(rows)
+
     imp = 0
-    for value in results.values():
-        p = (value/total)
-        imp -= (p*_log2(p))
+    for count in results.values():
+        p = count / total
+        imp -= p * _log2(p)
     return imp
 
 
-def _split_numeric(prototype: List, column: int, value):
-    return prototype[column] >= value
+def _split_numeric(prototype: List, column: int, value: int):
+    return prototipe[column] >= value
 
 
-def _split_categorical(prototype: List, column: int, value) -> bool:
-    return prototype[column] == value
-
+def _split_categorical(prototype: List, column: int, value: int):
+    raise NotImplementedError
 
 
 def divideset(part: Data, column: int, value: int) -> Tuple[Data, Data]:
@@ -93,14 +108,7 @@ def divideset(part: Data, column: int, value: int) -> Tuple[Data, Data]:
         split_function = _split_numeric
     else:
         split_function = _split_categorical
-
-    set1, set2 = list(), list()
-    for row in part:
-        if split_function(row,column,value):
-            set1.append(row)
-        else:
-            set2.append(row)
-
+    #...
     return (set1, set2)
 
 
@@ -124,76 +132,62 @@ class DecisionNode:
         self.fb = fb
 
 
+def _gain(part: Data, set1: Data, set2: Data, scoref):
+    p1 = len(set1) / len(part)
+    p2 = len(set2) / len(part)
+    return scoref(part) - p1 * scoref(set1) - p2 * scoref(set2)
+
+
 def buildtree(part: Data, scoref=entropy, beta=0):
     """
     t9: Define a new function buildtree. This is a recursive function
     that builds a decision tree using any of the impurity measures we
     have seen. The stop criterion is max_s\Delta i(s,t) < \beta
     """
-
-    """
-    1-Calcular impureza del node ; Nos la proporciona el codigo 
-    2-Decidir mejor pregunta
-    3-recursividad
-    """
     if len(part) == 0:
         return DecisionNode()
-    #1-Calcular impureza
+
     current_score = scoref(part)
-    #No usamos Beta
+
     if current_score == 0:
-        return DecisionNode(
-            results=unique_counts(part)
-        )
-    
+        # The partition is pure
+        return DecisionNode(results=unique_counts(part))
 
     # Set up some variables to track the best criteria
     best_gain = 0
     best_criteria = None
     best_sets = None
 
-    n_cols = len(part[0]) -1
+    n_cols = len(part[0]) - 1  # Skip the label
 
+    for i in range(n_cols):
+        possibles_cut_values = set()
+        for row in part:
+            possibles_cut_values.add(row[i])
 
-    #2-Buscar mejor pregunta
-    for col_idx in range(n_cols):
-        for value in _get_values(part,col_idx):
-            set1, set2 = divideset(part,col_idx,value)
-            dism_true = len(set1) / len(part) * scoref(set1)
-            dism_false = len(set2) / len(part) *scoref(set2)
-            gain = current_score - dism_true - dism_false
+        for value in possibles_cut_values:
+            set1, set2 = divideset(part, i, value)
+            gain = _gain(part, set1, set2, scoref)
             if gain > best_gain:
                 best_gain = gain
-                best_criteria = (col_idx,value)
-                best_sets = set1,set2
-    #Usamos beta, Logica de beta
+                best_criteria = (i, value)
+                best_sets = set1, set2
+
     if best_gain < beta:
-        return DecisionNode(results = unique_counts(part))
-    
-    set1,set2 = best_sets
-    col_idx,value = best_criteria
-    tb = buildtree(set1,scoref,beta)
-    fb = buildtree(set2,scoref,beta)
+        return DecisionNode(results=unique_counts(part))
 
-    return DecisionNode(col=col_idx,value=value,tb=tb,fb=fb)
-    
-    """
-    Scoref representa el indice de impureza
+    return DecisionNode(col=best_criteria[0], value=best_criteria[1],
+        tb=buildtree(best_sets[0]), fb=buildtree(best_sets[1]))
 
-    Cada particion disminuye la impureza por ello a mas ganancia, la impureza estara mas reduida.
-    Valor de goodnes mas grande *Apuntes*
-    """
-
-def _get_values(rows,col_idx):
-    value = set()
-    for row in rows:
-        value.add(row[col_idx])
-    return value
 
 def iterative_buildtree(part: Data, scoref=entropy, beta=0):
     """
     t10: Define the iterative version of the function buildtree
     """
+    raise NotImplementedError
+
+
+def classify(tree, values):
     raise NotImplementedError
 
 
@@ -203,7 +197,7 @@ def print_tree(tree, headers=None, indent=""):
     """
     # Is this a leaf node?
     if tree.results is not None:
-        print(tree.results)
+        print(indent, tree.results)
     else:
         # Print the criteria
         criteria = tree.col
@@ -218,45 +212,35 @@ def print_tree(tree, headers=None, indent=""):
         print_tree(tree.fb, headers, indent + "  ")
 
 
+def print_data(headers, data):
+    colsize = 15
+    print('-' * ((colsize + 1) * len(headers) + 1))
+    print("|", end="")
+    for header in headers:
+        print(header.center(colsize), end="|")
+    print("")
+    print('-' * ((colsize + 1) * len(headers) + 1))
+    for row in data:
+        print("|", end="")
+        for value in row:
+            if isinstance(value, (int, float)):
+                print(str(value).rjust(colsize), end="|")
+            else:
+                print(value.ljust(colsize), end="|")
+        print("")
+    print('-' * ((colsize + 1) * len(headers) + 1))
+
+
 def main():
-    try:   
+    try:
         filename = sys.argv[1]
     except IndexError:
         filename = "decision_tree_example.txt"
-    headres,data = read(filename)
-    
-    for value in ["USA","France","UK","NewZealand"]:
-        set1, set2 = divideset(data,1,value)
-        print("Split by:",value)
-        print("gini set1:",gini_impurity(set1))
-        print("gini set2:",gini_impurity(set2))
-        print("entropy set1:",entropy(set1))
-        print("entropy set2:",entropy(set2))
+
+    headers, data = read(filename)
     tree = buildtree(data)
-    print_tree(tree,headres)
+    print_tree(tree, headers)
 
 
-    """
-    print(headres)
-        for row in data:
-            print(row)
-
-        print("-----")
-        print(gini_impurity(data))
-        print(entropy(data))
-    """
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
-
-
-"""
-1: caluclar impureza de nodo (i)
-2: escoger pregunta
-|   for query in possible_query():
-|       set1,set2 = divide(query,data)
-Calcular metrica para saber si la pregunta es buena o mala
-|       function(data,set1,set2)
-3: Generar hijos
-|   buildTree(set1)
-|   buildTree(set2)
-"""
